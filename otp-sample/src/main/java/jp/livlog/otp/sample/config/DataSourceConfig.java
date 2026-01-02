e27f3d8873873568ed30f9ec16295b9f2683cee0
HIDEKAZU AOSHIMA <h.aoshima@livlog.jp>
2026-01-02 21:32:29 +0900

    Add fallback embedded DataSource for sample app

--- otp-sample/src/main/java/jp/livlog/otp/sample/config/DataSourceConfig.java
+++ otp-sample/src/main/java/jp/livlog/otp/sample/config/DataSourceConfig.java
@@ -1,8 +1,33 @@
 package jp.livlog.otp.sample.config;
 
+import org.h2.jdbcx.JdbcDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DataSourceConfig {
-    // Spring Boot + H2 auto-config に任せる（Bean不要）
+
+    @Bean
+    public DataSource dataSource(
+            @Value("${spring.datasource.url:}") String url,
+            @Value("${spring.datasource.username:}") String username,
+            @Value("${spring.datasource.password:}") String password) {
+        if (url != null && !url.isBlank()) {
+            JdbcDataSource ds = new JdbcDataSource();
+            ds.setURL(url);
+            ds.setUser(username);
+            ds.setPassword(password);
+            return ds;
+        }
+
+        return new EmbeddedDatabaseBuilder()
+                .generateUniqueName(true)
+                .setType(EmbeddedDatabaseType.H2)
+                .build();
+    }
 }
