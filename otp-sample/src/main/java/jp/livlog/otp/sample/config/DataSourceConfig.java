0e41a13cc914bbada9550aec00600038d5db5c61
HIDEKAZU AOSHIMA <h.aoshima@livlog.jp>
2026-01-02 21:15:12 +0900

    Remove custom DataSource bean from sample app

--- otp-sample/src/main/java/jp/livlog/otp/sample/config/DataSourceConfig.java
+++
@@ -1,8 +0,0 @@
-package jp.livlog.otp.sample.config;
-
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class DataSourceConfig {
-    // Spring Boot + H2 auto-config に任せる（Bean不要）
-}
